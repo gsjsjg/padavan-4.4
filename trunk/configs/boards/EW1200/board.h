baa0d009602c2b086882375b9865f465ecf7d57b
X.Joe <38424730+dearxjoe@users.noreply.github.com>
2023-01-15 01:43:35 +0800

    Update board.h

--- trunk/configs/boards/EW1200/board.h
+++ trunk/configs/boards/EW1200/board.h
@@ -1,25 +1,26 @@
-/* Afoundry EW1200 edit by dearjoe@qq.com*/
+/* Afoundry EW1200 */
 
 #define BOARD_PID		"EW1200"
 #define BOARD_NAME		"EW1200"
 #define BOARD_DESC		"Afoundry EW1200 Wireless Router"
-#define BOARD_VENDOR_NAME	"Afoundry Computer Inc."
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
 #define BOARD_VENDOR_URL	"https://nas.dearjoe.top/"
 #define BOARD_MODEL_URL		"https://nas.dearjoe.top/"
 #define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	41
-#define BOARD_GPIO_BTN_WPS	43
+#define BOARD_GPIO_BTN_RESET	18
+#undef  BOARD_GPIO_BTN_WPS	
 #define BOARD_GPIO_LED_ALL	46
 #undef  BOARD_GPIO_LED_WIFI
 #define BOARD_GPIO_LED_SW2G	14
-#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_SW5G 15 //?
 #define BOARD_GPIO_LED_POWER	48
 #define BOARD_GPIO_LED_LAN	7
 #define BOARD_GPIO_LED_WAN	16
-#define BOARD_GPIO_LED_USB	47
-#undef  BOARD_GPIO_LED_ROUTER
-#undef  BOARD_GPIO_PWR_USB
+#define BOARD_GPIO_LED_USB	4
+#define BOARD_GPIO_LED_ROUTER   5
+#define BOARD_GPIO_PWR_USB  11    //dearjoe add
+#define BOARD_GPIO_PWR_USB_ON	1  //dearjoe add
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
